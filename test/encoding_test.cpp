dca8995016da6ffc12707585d907dadb7f759518
ritterwu <ritterwu@tencent.com>
2020-08-04 10:53:49 +0800

    add the test demo

---
+++ test/encoding_test.cpp
@@ -0,0 +1,17 @@
+#include "..\include\little_helper.hpp"
+
+#include <iostream>
+#include <string>
+
+using namespace little_helper;
+
+int main()
+{
+    std::string str_u8 = u8"hello world";
+    // std::wstring wstr_u8 = encoding::from_utf8(str_u8);
+
+    // std::wcout << wstr_u8 << std::endl;
+
+    std::cout << str_u8 << std::endl;
+    return 0;
+}
