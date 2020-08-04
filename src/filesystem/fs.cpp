fea5a1c34093cf5fc5601878fe5c414dc30c6bd9
ritterwu <ritterwu@tencent.com>
2020-08-04 10:52:36 +0800

    add the filesystem api

---
+++ src/filesystem/fs.cpp
@@ -0,0 +1,8 @@
+#include "fs.h"
+
+using namespace little_helper;
+
+#if __cplusplus > 201703L
+
+
+#endif
---
+++ src/filesystem/fs.h
@@ -0,0 +1,19 @@
+#ifndef _INCLUDE_LH_FILESYSTEM_
+#define _INCLUDE_LH_FILESYSTEM_
+
+#include <vector>
+
+#include "../../include/lh_config.h"
+
+#if __cplusplus > 201703L
+#include <filesystem>
+#endif
+
+namespace little_helper {
+namespace fs {
+
+
+}
+}
+
+#endif // _INCLUDE_LH_NETWORK_
